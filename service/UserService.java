f34b56bc4a776a02c460fca94fc17580fa7077b8
divekarsanket <106264546+divekarsanket@users.noreply.github.com>
2026-03-12 01:05:56 +0530

    Add files via upload

---
+++ service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.pro.service;
+
+
+import com.example.pro.model.User;
+import com.example.pro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id).orElse(null);;
+
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+        user.setDepartment(userDetails.getDepartment());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
