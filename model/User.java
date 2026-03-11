1e5c8dd2bf39a308b1f71cded91c55c5297fa2e2
divekarsanket <106264546+divekarsanket@users.noreply.github.com>
2026-03-12 01:04:17 +0530

    Add files via upload

---
+++ model/User.java
@@ -0,0 +1,23 @@
+package com.example.pro.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+}
